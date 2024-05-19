e03097d66f9850b3a831a4cdf114f221955b95b9
Tanmay Gharat <142776418+TanmayGharat1505@users.noreply.github.com>
2024-05-20 00:28:34 +0530

    Add files via upload

---
+++ TS1.js
@@ -0,0 +1,10 @@
+var person = /** @class */ (function () {
+    function person() {
+    }
+    return person;
+}());
+var obj = new person();
+obj.name = "Anish";
+obj.city = "Mumbai";
+console.log(obj.name);
+console.log(obj.city);
